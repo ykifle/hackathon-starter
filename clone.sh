869145baf18e18f54b9797e6a2ac0a637b8efcef
Ubuntu <ubuntu@ip-10-10-11-179.us-west-2.compute.internal>
2016-09-11 11:24:47 +0000

    add a script for calling clone rest services w

--- clone.sh
+++ clone.sh
@@ -1,3 +1,10 @@
-cloneNum = $RANDOM
+#!/bin/bash
+cloneNum=$RANDOM
+echo 'clone='$cloneNum
 curl 'https://52.32.148.251:8443/api/1.0/ontap/volumes/d014ace2-6f03-11e6-8b3b-4d8e0894f7d7%3Atype%3Dvolume%2Cuuid%3D206b0cfe-c2a6-44d8-a1d2-b8a43f715b11/jobs/clone' -H 'Origin: https://52.32.148.251:8443' -H 'Authorization: Basic YWRtaW46TmV0YXBwMSE=' -H 'Content-Type: application/json' -H 'Accept: application/json' --data-binary $'{\n  "volume_clone_name": "clone-'$RANDOM'"\n}' --compressed --insecure
-sleep 3
+sleep 5
+echo $cloneNum > /var/cardinals/mocha.txt
+docker run --volume-driver netapp -v clone:/clone turboestate
+cat /var/cardinals/mocha.txt
+sleep 5
+docker volume rm clone$RANDOM
